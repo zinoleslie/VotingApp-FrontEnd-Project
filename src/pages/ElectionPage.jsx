0a94024be5206903fa702057fc1c0b4a12245d77
zinoleslie <giftedzinoleslie@gmail.com>
2025-01-23 14:52:14 +0100

    candidate page created with toggle comfirm page with redux

---
+++ src/pages/ElectionPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ElectionPage = () => {
+  return (
+    <div>ElectionPage</div>
+  )
+}
+
+export default ElectionPage
---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import uiSlice from "./ui-Slice";
+
+
+
+
+
+
+
+const store = configureStore({
+    reducer: { ui: uiSlice.reducer }
+})
+
+export default store
